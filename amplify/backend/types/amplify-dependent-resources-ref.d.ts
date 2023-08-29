67132255e4df58bebbee69130c26d101f5b6f10d
yuma-ueki <yuma-ueki@kccs.co.jp>
2023-08-29 15:44:09 +0900

    henkou

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "uekiyumapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
