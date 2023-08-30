53e2797d55c53d32642627acf415884de0ee8319
yuma-ueki <yuma-ueki@kccs.co.jp>
2023-08-30 10:51:58 +0900

    hennkou

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import logo from './logo.svg';
+import React, { useEffect, useReducer } from 'react';
+
+import API, { graphqlOperation } from '@aws-amplify/api';
+import PubSub from '@aws-amplify/pubsub';
+
+import { createTodo } from './graphql/mutations';
+import { listTodos } from './graphql/queries';
+import { onCreateTodo } from './graphql/subscriptions';
+
+import awsconfig from './aws-exports';
 import './App.css';
 
-//import API, { graphqlOperation } from '@aws-amplify/api';
+API.configure(awsconfig);
+PubSub.configure(awsconfig);
+
+// Action Types
+const QUERY = 'QUERY';
+const SUBSCRIPTION = 'SUBSCRIPTION';
+
+const initialState = {
+  todos: [],
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case QUERY:
+      return {...state, todos: action.todos};
+    case SUBSCRIPTION:
+      return {...state, todos:[...state.todos, action.todo]}
+    default:
+      return state;
+  }
+};
+
+async function createNewTodo() {
+  const todo = { name: "Use AWS AppSync", description: "RealTime and Offline" };
+  await API.graphql(graphqlOperation(createTodo, { input: todo }));
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    async function getData() {
+      const todoData = await API.graphql(graphqlOperation(listTodos));
+      dispatch({ type: QUERY, todos: todoData.data.listTodos.items });
+    }
+    getData();
+
+    const subscription = API.graphql(graphqlOperation(onCreateTodo)).subscribe({
+      next: (eventData) => {
+        const todo = eventData.value.data.onCreateTodo;
+        dispatch({ type: SUBSCRIPTION, todo });
+      }
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
   return (
-
-<div>データ作成
-    <label for="title">タイトル</label>
-    <input type="text" id="title" value="foo" />
-    <label for="content">内容</label>
-    <input type="text" id="content" value="bar" />
-
-    Create button
-    <button  /* id="create" */  >Create</button>
-
-  </div>
-
-  
-
-
-
+    <div className="App">
+      <button onClick={createNewTodo}>Add Todo</button>
+      <div>
+        {state.todos.length > 0 ? 
+          state.todos.map((todo) => <p key={todo.id}>{todo.name} : {todo.description}</p>) :
+          <p>Add some todos!</p> 
+        }
+      </div>
+    </div>
   );
 }
 
