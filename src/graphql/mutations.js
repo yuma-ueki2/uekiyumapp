a2fe3d58a415417231924768cd5c4675df84bb56
yuma-ueki <yuma-ueki@kccs.co.jp>
2023-08-30 10:57:36 +0900

    henkou

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -1,15 +1,23 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const createTodo = `mutation CreateTodo($input: TodoInput!) {
-  createTodo(input: $input) {
-    title
-    content
+export const createTodo = /* GraphQL */ `
+  mutation CreateTodo(
+    $input: CreateTodoInput!
+    $condition: ModelTodoConditionInput
+  ) {
+    createTodo(input: $input, condition: $condition) {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+      __typename
+    }
   }
-}
-`;
-//export const updateTodo = /* GraphQL */ `
-/*  mutation UpdateTodo(
+  `;
+export const updateTodo = /* GraphQL */ `
+  mutation UpdateTodo(
     $input: UpdateTodoInput!
     $condition: ModelTodoConditionInput
   ) {
@@ -23,11 +31,8 @@ export const createTodo = `mutation CreateTodo($input: TodoInput!) {
     }
   }
 `;
-
-
-//export const deleteTodo = /* GraphQL */ /*    `
-  
-mutation DeleteTodo(
+export const deleteTodo = /* GraphQL */ `
+  mutation DeleteTodo(
     $input: DeleteTodoInput!
     $condition: ModelTodoConditionInput
   ) {
@@ -40,4 +45,7 @@ mutation DeleteTodo(
       __typename
     }
   }
-`; */
+`;
+
+
+
